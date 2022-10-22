4635aa79aaff86e0ea71c0f20e2a961a286262a7
naffiziqbal <f.nafiziqbal@gmail.com>
2022-10-22 18:40:50 +0600

    User Registration Page Design

--- src/User/Login/Login.jsx
+++ src/User/Login/Login.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
 import { AuthContext } from '../../UserContext/UserContext';
 
 const Login = () => {
@@ -6,7 +7,49 @@ const Login = () => {
     console.log(user);
     return (
         <div>
-            This IS Log in Page
+                 <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col">
+          <div className="text-center">
+            <h1 className="text-5xl font-bold">Log In Now</h1>
+          </div>
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <div className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="email"
+                  name="email"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="password"
+                  name="password"
+                  className="input input-bordered"
+                />
+                <label className="label"></label>
+                <p>
+                  Don't Have an Account?{" "}
+                  <Link className="text-yellow-500" to="/signup">
+                   Register Now
+                  </Link>
+                </p>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary text-white">Log In</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
         </div>
     );
 };
--- src/User/SignUp/SignUp.jsx
+++ src/User/SignUp/SignUp.jsx
@@ -1,15 +1,67 @@
-import React, { useContext } from 'react';
-import { AuthContext } from '../../UserContext/UserContext';
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../UserContext/UserContext";
 
 const SignUp = () => {
-    const {user } = useContext(AuthContext);
-    console.log(user);
-    
-    return (
-        <div>
-            This  Is Sign Up Page
+  const { user } = useContext(AuthContext);
+  console.log(user);
+
+  return (
+    <div>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col">
+          <div className="text-center">
+            <h1 className="text-5xl font-bold">Register Now</h1>
+          </div>
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <div className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input
+                  type="namea"
+                  placeholder="Your Name"
+                  name="name"
+                  className="input input-bordered"
+                />
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="email"
+                  name="email"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="password"
+                  name="password"
+                  className="input input-bordered"
+                />
+                <label className="label"></label>
+                <p>
+                  Already Have an Account?{" "}
+                  <Link className="text-yellow-500" to="/login">
+                    Log In Now
+                  </Link>
+                </p>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary text-white">Sign Up</button>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;

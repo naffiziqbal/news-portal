ae015b2855b47fc10eee27f70a3be974177abd96
naffiziqbal <f.nafiziqbal@gmail.com>
2022-10-22 20:12:34 +0600

    User Log in Using Google and Github

--- src/User/SignUp/SignUp.jsx
+++ src/User/SignUp/SignUp.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { AuthContext } from "../../UserContext/UserContext";
 
 const SignUp = () => {
-  const { user, createUser } = useContext(AuthContext);
+  const { user, createUser, googleSignUp, UserLogInGitHub } = useContext(AuthContext);
   console.log(user);
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -12,22 +12,43 @@ const SignUp = () => {
     const email = form.email.value;
     const password = form.password.value;
     createUser(email, password)
-    .then( (result)=>{
+      .then((result) => {
         const user = result.user;
         console.log(user);
         form.reset();
-        setUser(user)
-    } )
-    .catch((err)=> {
+        setUser(user);
+      })
+      .catch((err) => {
         console.log(err.message);
-        setUser(null)
-
-        
-    })
-    
+        setUser(null);
+      });
 
     alert("Form Submit");
   };
+  //   Log In Using Google 
+  const handleLogInWithGoogle = ()=> {
+    googleSignUp()
+    .then((result) => {
+        const user = result.user;
+        setUser(user);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
+//   Log In Using Github 
+  const handleLogInWithGithub = ()=> {
+    UserLogInGitHub()
+    .then((result) => {
+        const user = result.user;
+        setUser(user);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+  
 
   return (
     <div>
@@ -77,16 +98,28 @@ const SignUp = () => {
                 </p>
               </div>
               <div className="form-control mt-6">
-                <button className="btn btn-primary text-white" type="submit">Sign Up</button>
+                <button className="btn btn-primary text-white" type="submit">
+                  Sign Up
+                </button>
               </div>
             </form>
             <div className="thirdParty flex justify-center flex-col p-4">
-            <button className="my-2 border border-yellow-400 flex items-center rounded-lg justify-evenly hover:bg-slate-200 duration-300">
-                <img className="w-12 rounded-full" src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/d2abd662597191.5a9589b09ddf5.jpg" alt="" />
-                Log In With Google</button>
-            <button className="border border-yellow-400 flex items-center rounded-lg justify-evenly hover:bg-slate-200 duration-300">
-                <img className="w-8 rounded-full"  src="https://www.kindpng.com/picc/m/718-7187053_close-eyes-vector-github-logo-icon-png-transparent.png" alt="" />
-                Log In With Github</button>
+              <button onClick={handleLogInWithGoogle} className="my-2 border border-yellow-400 flex items-center rounded-lg justify-evenly hover:bg-slate-200 duration-300">
+                <img
+                  className="w-12 rounded-full"
+                  src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/d2abd662597191.5a9589b09ddf5.jpg"
+                  alt=""
+                />
+                Log In With Google
+              </button>
+              <button onClick={handleLogInWithGithub} className="border border-yellow-400 flex items-center rounded-lg justify-evenly hover:bg-slate-200 duration-300">
+                <img
+                  className="w-8 rounded-full"
+                  src="https://www.kindpng.com/picc/m/718-7187053_close-eyes-vector-github-logo-icon-png-transparent.png"
+                  alt=""
+                />
+                Log In With Github
+              </button>
             </div>
           </div>
         </div>
